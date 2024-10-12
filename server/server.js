df73b9faffb1e06e40e0db34a079928d49671606
kmrahul17 <rahul02.bunny@gmail.com>
2024-10-12 22:13:16 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,6 +10,12 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const corsOptions = {
+     origin: process.env.FRONTEND_URL || 'http://localhost:3001',
+     credentials: true,
+     optionsSuccessStatus: 200
+};
+
 app.use(cors());
 app.use(express.json());
 
